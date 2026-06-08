df61a57da9303848e1f2167b1ab84f482a6a1e1d
Sim1Iarity <lufm1510@LAPTOP-EJK3F4BP.localdomain>
2026-06-08 13:22:23 -0400

    Add bit reversal and few unique generators

--- gens.hpp
+++ gens.hpp
@@ -202,6 +202,32 @@ public:
     }
 };
 
+class BitReverseIndices : public genIndices {
+    unsigned int bit_reverse(unsigned int x) {
+        x = (((x & 0xaaaaaaaa) >> 1) | ((x & 0x55555555) << 1));
+        x = (((x & 0xcccccccc) >> 2) | ((x & 0x33333333) << 2));
+        x = (((x & 0xf0f0f0f0) >> 4) | ((x & 0x0f0f0f0f) << 4));
+        x = (((x & 0xff00ff00) >> 8) | ((x & 0x00ff00ff) << 8));
+        return((x >> 16) | (x << 15));
+    }
+public:
+    void generate(myElement* indices, int n) override {
+        for (int i = 0; i < n; i++) {
+            indices[i] = bit_reverse(i);
+        }
+    }
+};
+
+class FewUniqueIndices : public genIndices {
+public:
+    void generate(myElement* indices, int n) override {
+        RandomNumberGenerator rng(std::random_device{}());
+        for (int i = 0; i < n; i++) {
+            indices[i] = rng() % 256;
+        }
+    }
+};
+
 const std::pair<genIndices*, std::string> generators[] =
 {
     {new RandomIndices(), "random"},
@@ -216,7 +242,9 @@ const std::pair<genIndices*, std::string> generators[] =
     {new ScrambledHeadIndices(), "scrambled head"},
     {new ScrambledTailIndices(), "scrambled tail"},
     {new HalfReverseIndices(), "half-reversed"},
-    {new AdversaryIndices(), "adversary"}
+    {new AdversaryIndices(), "adversary"},
+    {new BitReverseIndices(), "bit reversal"},
+    {new FewUniqueIndices(), "few unique"}
 };
 
 const std::string genNames[] =
@@ -233,5 +261,7 @@ const std::string genNames[] =
     "randhead",
     "randtail",
     "halfrev",
-    "adversary"
+    "adversary",
+    "bitrev",
+    "fewuniq"
 };
